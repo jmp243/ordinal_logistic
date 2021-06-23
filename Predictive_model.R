a8090594562c2edf84db7c6516ba1763497b5594
Jung Mee Park <jpark1917@gmail.com>
2021-06-22 19:17:35 -0700

    work in progress

---
+++ Predictive_model.R
@@ -0,0 +1,12 @@
+# Jung Mee Park
+# first sync R to github
+# https://jcoliver.github.io/learn-r/010-github.html
+
+# build a predictive model around Q30
+
+library(VIM)
+library("mice")
+library(vioplot)
+library(ISLR)
+library(caTools)
+library(neuralnet)
